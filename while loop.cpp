50df74579cd61b8c41a7b474d7cdb9106b80d4b4
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-07-29 23:56:52 -0700

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int i=1;
+int j;
+int k=1;
+cout<<"Enter your number: \n";
+cin>>j;
+cout<<"Enter your limit\n";
+cin>>k;
+while(i<=k)
+{
+	cout<<j<<" X "<<i<<" = "<<i*j<<endl;
+	i++;
+}
+
+return 0;
+}
