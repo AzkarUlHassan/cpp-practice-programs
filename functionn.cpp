b34bc108f57a824a6043d38f6b7cde6cb01fb71d
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-12 17:29:02 +0500

    Add files via upload

---
+++ functionn.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+void line();
+void line()
+{
+   cout<<"________________________";	
+}
+int main()
+{
+line();
+cout<<"\nHelo\n";
+line();
+cout<<"\nWhere are you from dear\n";
+line();
+}
