86cd893cfa76a2a1cba1e58f33a9cfa02faf7ba8
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-17 22:19:47 +0500

    Add files via upload

---
+++ pass by value pass by refrence.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void sum(int num1, int num2);
+void sum(int num1, int num2)
+{
+	cout<<"The sum is "<<num1+num2;
+};
+int main()
+{
+int n1, n2;
+cout<<"Enter first number :";
+cin>>n1;
+cout<<"Enter first number :";
+cin>>n2;
+sum(n1,n2);
+return 0;
+}
+
