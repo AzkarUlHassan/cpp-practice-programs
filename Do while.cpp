7e4abac0b272f87c3beed34e03f1743340fa2694
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-17 22:27:39 +0500

    Add files via upload

---
+++ Do while.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a=1;
+	do
+	{
+		cout<<a<<"\t"<<(11-a)<<endl;
+		a++;
+		
+	}
+	while(a<=10);
+
+
+return 0;
+}
+
