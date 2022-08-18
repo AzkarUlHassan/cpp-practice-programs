d704cd158de187e45af3ecfbc21928d3cff4c8e9
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-18 20:40:34 +0500

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int a[5];
+int main()
+{
+for(int i=0;i<5;i++)
+{
+	cout<<"Put values in array : ";
+	cin>>a[i];	
+}
+cout<<"The sum is "<<a[0]+a[1]+a[2]+a[3]+a[4];
+
+}
+
