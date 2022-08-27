4c7addb862c4511c6469adc1d6ad1034bc51ee41
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-27 09:26:24 +0500

    Add files via upload

---
+++ enumerated data type.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+enum week{sunday,monday,tuesday,wednesday,thirsday,friday,saturday};
+int main()
+{
+week today;
+today=friday;
+cout<<"Today is "<<today+1<<"th day of week ";
+
+return 0;
+}
+
