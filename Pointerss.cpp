4c61c73519cec92556c0f35f2221125ec93d09d1
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-08-31 20:16:36 +0500

    Add files via upload

---
+++ Pointerss.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int var1 =12;
+	int var2 =34;
+	int var3=56;
+	cout<<"Adress of variable 1 is "<<&var1<<endl;
+	cout<<"Adress of variable 2 is "<<&var2<<endl;
+	cout<<"Adress of variable 3 is "<<&var3<<endl;
+}
+
